20c969f6a64ec0529dae02c995a51192b7fe734d
Lohana Torres <lt.leopoldo@gmail.com>
2020-11-26 22:21:08 -0300

    🔥 Remove comment

--- src/index.js
+++ src/index.js
@@ -2,9 +2,3 @@ import generatePwdForm from './modules/generatePwdForm';
 import './assets/css/style.css';
 
 generatePwdForm();
-
-/* document.addEventListener('click', (e) => {
-  const el = e.target;
-
-  if (!el.classList.contains('generate')) return;
-}); */
